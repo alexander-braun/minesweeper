7c75564491a23a3762c01545b6f39f562c257637
alexander-braun <alexanderbraun@freenet.de>
2020-08-22 17:41:21 +0200

    optimized html semantics with nav component instead of div

--- client/src/components/Side/styles/elements.js
+++ client/src/components/Side/styles/elements.js
@@ -230,7 +230,7 @@ export const NoRecordholderWrapper = styled.div`
   }
 `;
 
-export const SelectMenueWrapper = styled.div`
+export const SelectMenueWrapper = styled.nav`
   width: 100%;
   display: flex;
   flex-direction: column;
