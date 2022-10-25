19ab99d56ab18a136126e873f4599ceb3e502172
alexander-braun <alexanderbraun@freenet.de>
2022-10-25 21:39:39 +0200

    fix: build script

--- client/src/components/Side/Side.js
+++ client/src/components/Side/Side.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import Bestlist from './Bestlist';
-import SubmitTime from './SubmitTime';
-import { Sidewrapper } from './styles/elements';
-import { useSelector } from 'react-redux';
+import React from "react";
+import SubmitTime from "./SubmitTime";
+import { Sidewrapper } from "./styles/elements";
+import { useSelector } from "react-redux";
 
 function Side() {
   const gameState = useSelector((state) => state.gameState);
   const genSide = () => {
-    if (gameState === 'win') {
+    if (gameState === "win") {
       return (
         <Sidewrapper submit>
           <SubmitTime />
